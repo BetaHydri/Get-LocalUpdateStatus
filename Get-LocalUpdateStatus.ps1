2ef00a4ef04bb733b494dfa90299b30f3027d3bd
BetaHydri <jan_tiedemann@hotmail.com>
2025-10-01 15:41:04 +0200

    new update source regcheck

--- Get-LocalUpdateStatus.ps1
+++ Get-LocalUpdateStatus.ps1
@@ -1,7 +1,7 @@
+<#
+PSScriptInfo
 
-<#PSScriptInfo
-
-.VERSION 1.0.2
+.VERSION 1.0.3
 
 .GUID 4b937790-b06b-427f-8c1f-565030ae0227
 
@@ -13,99 +13,56 @@
 
 .TAGS Updates, WindowsUpdates
 
-.LICENSEURI
-
-.PROJECTURI
-
-.ICONURI
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS
-
-.EXTERNALSCRIPTDEPENDENCIES
-
-.RELEASENOTES
-
-
-.PRIVATEDATA
-
+.DESCRIPTION 
+Enumerates missing or installed Windows Updates and returns an array of objects with update details.
 #>
 
-<# 
-
-.DESCRIPTION 
- Enumerates missing or Installed Windows Updates and returns an array of Objects with updates 
-
-#> 
-
-
-function Get-LocalUpdateStatus { 
+function Get-LocalUpdateStatus {
   #requires -Version 4
-  <#
-    .SYNOPSIS
-		Script to search for security updates installed and/or missing
-	
-    .NOTES
-		Jan-Andre Tiedemann
-		    
-    .DESCRIPTION
-		A  script to search for missing software updates without CAB
-    
-    .PARAMETER 	ComputerName
-		The machine as NetBIOS or FQDN
-    
-    .PARAMETER UpdateSearchFilter
-    Predefined ValidateSet
-
-    .EXAMPLE
-		Get-LocalUpdateStatus -ComputerName 'Server1'
-	
-	.EXAMPLE
-		Get-LocalUpdateStatus -ComputerName $env:ComputerName -UpdateSearchFilter 'IsHidden=0 and IsInstalled=0'
-    
-	.OUTPUTS
-		System.String. You can pipe it into a Table and wirte it into a csv for further excel processing.
-  #>
   [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
   param (
-    # Target machine name
-    [Parameter(Position = 0, 
-      Mandatory = $true, 
-      ParameterSetName = 'ComputerName', 
-      HelpMessage = "Enter target machine name")]
+    [Parameter(Position = 0, Mandatory = $true, ParameterSetName = 'ComputerName')]
     [System.String]$ComputerName,
 
-    # Target machines as text file each line one server
-    #[Parameter(Position=0, 
-    #Mandatory = $true, 
-    #ParameterSetName = 'ServerFile', 
-    #HelpMessage="Target machines as text file. Each line one server")]
-    #[System.String]$Servers_file,
-
-    # UpdateSearchFilter e.g. 'IsHidden=0 and IsInstalled=0' to retrieve 'Missing Updates' including hidden ones.
-    [Parameter(Position = 1, 
-      Mandatory = $true,
-      HelpMessage = "Filter to show e.g. only missing updates 'IsInstalled=0' or all updates 'IsHidden=0'")]
+    [Parameter(Position = 1, Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
     [ValidateSet('IsHidden=0 and IsInstalled=0', 'IsHidden=0 and IsInstalled=1', 'IsInstalled=1', 'IsInstalled=0', 'IsHidden=0', 'IsHidden=1')]
     [System.String]$UpdateSearchFilter
-	
   )
 
-  #Check is Powershell was opened with Admin privileges
-  If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {    
+  # Check for admin privileges
+  If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "This script needs to be run As Admin" -ForegroundColor Red
-    Write-Host "Furthermore the User should be Admin on each Computer/Server from where you want to gather the WindowsUpdate status !" -ForegroundColor Yellow
+    Write-Host "Furthermore, the user should be Admin on each computer/server from where you want to gather the Windows Update status!" -ForegroundColor Yellow
     Break
   }
 
-  [void][Reflection.Assembly]::LoadFrom("C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\Microsoft.VisualBasic.dll") 
-  $session = [microsoft.visualbasic.interaction]::CreateObject("Microsoft.Update.Session", $ComputerName) 
-  $searcher = $session.CreateUpdateSearcher() 
+  # Display Windows Update source
+  $wuRegPath = "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate"
+  if (Test-Path $wuRegPath) {
+    $wuSettings = Get-ItemProperty -Path $wuRegPath
+    $wuServer = $wuSettings.WUServer
+    $wuStatusServer = $wuSettings.WUStatusServer
+
+    if ($wuServer) {
+      Write-Host "`nWindows Update is configured to use WSUS:" -ForegroundColor Cyan
+      Write-Host "WUServer      : $wuServer"
+      Write-Host "WUStatusServer: $wuStatusServer`n"
+    }
+    else {
+      Write-Host "`nWindows Update is using Microsoft Update (default)" -ForegroundColor Cyan
+    }
+  }
+  else {
+    Write-Host "`nWindows Update is using Microsoft Update (default)" -ForegroundColor Cyan
+  }
+
+  [void][Reflection.Assembly]::LoadFrom("C:\Windows\Microsoft.NET\Framework\v4.0.30319\Microsoft.VisualBasic.dll")
+  $session = [microsoft.visualbasic.interaction]::CreateObject("Microsoft.Update.Session", $ComputerName)
+  $searcher = $session.CreateUpdateSearcher()
   $results = $searcher.Search($UpdateSearchFilter)
 
-  # Enum for Severity number property
+  # Enum for Severity
   Add-Type -TypeDefinition '
   public enum MsrcSeverity {
     Unspecified,
@@ -113,12 +70,10 @@ function Get-LocalUpdateStatus {
     Moderate,
     Important,
     Critical
-  } ' -ErrorAction SilentlyContinue
-  
-  # Array to store Updates Objects
+  }' -ErrorAction SilentlyContinue
+
   $MyUpdates = @()
 
-  #$results=Get-LocalUpdateStatus $env:computername
   foreach ($update in $results.Updates) {
     $downloadUrl = $update.BundledUpdates | ForEach-Object {
       $_.DownloadContents | ForEach-Object {
@@ -127,42 +82,38 @@ function Get-LocalUpdateStatus {
     } | Select-Object -First 1
 
     $severity = 0
-
     try {
       $severity = ([int][MsrcSeverity]$update.MsrcSeverity)
-    }
-    catch { 
-
-    }
+    } catch {}
 
     $bulletinId = ($update.SecurityBulletinIDs | Select-Object -First 1)
     $bulletinUrl = if ($bulletinId) {
       'http://www.microsoft.com/technet/security/bulletin/{0}.mspx' -f $bulletinId
-    }
-    else {
+    } else {
       [System.String]::Empty
-    }    
+    }
+
     $updates = New-Object -TypeName psobject |
-    Add-Member -MemberType NoteProperty -Name Computer -Value "$env:computername" -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name Id -Value ($update.SecurityBulletinIDs | Select-Object -First 1) -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name CVEIds -Value ($update.cveids | Select-Object -First 1) -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name BulletinId -Value $bulletinId -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name KbId -Value ($update.KBArticleIDs | Select-Object -First 1) -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name Type -Value $update.Type -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name IsInstalled -Value $update.IsInstalled -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name InstalledOn -Value $update.LastDeploymentChangeTime -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name RestartRequired -Value $update.RebootRequired -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name Title -Value $update.Title -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name Description -Value $update.Description -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name SeverityText -Value ([MsrcSeverity][int]$severity) -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name Severity -Value $severity -PassThru -ErrorAction SilentlyContinue -Force |
-    Add-Member -MemberType NoteProperty -Name InformationURL -Value ($update.MoreInfoUrls | Select-Object -First 1) -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name SupportURL -Value $update.supporturl -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name DownloadURL -Value $downloadUrl -PassThru -Force |
-    Add-Member -MemberType NoteProperty -Name BulletinURL -Value $bulletinUrl -PassThru -Force
+      Add-Member -MemberType NoteProperty -Name Computer -Value "$env:computername" -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name Id -Value ($update.SecurityBulletinIDs | Select-Object -First 1) -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name CVEIds -Value ($update.cveids | Select-Object -First 1) -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name BulletinId -Value $bulletinId -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name KbId -Value ($update.KBArticleIDs | Select-Object -First 1) -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name Type -Value $update.Type -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name IsInstalled -Value $update.IsInstalled -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name InstalledOn -Value $update.LastDeploymentChangeTime -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name RestartRequired -Value $update.RebootRequired -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name Title -Value $update.Title -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name Description -Value $update.Description -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name SeverityText -Value ([MsrcSeverity][int]$severity) -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name Severity -Value $severity -PassThru -ErrorAction SilentlyContinue -Force |
+      Add-Member -MemberType NoteProperty -Name InformationURL -Value ($update.MoreInfoUrls | Select-Object -First 1) -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name SupportURL -Value $update.supporturl -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name DownloadURL -Value $downloadUrl -PassThru -Force |
+      Add-Member -MemberType NoteProperty -Name BulletinURL -Value $bulletinUrl -PassThru -Force
 
     $MyUpdates += $updates
   }
-    
-  $MyUpdates 
+
+  $MyUpdates
 }
